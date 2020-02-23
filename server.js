0b182fc05b7b4d686ad9a981828b10609d5d1295
PvasquezF <201503611@ingenieria.usac.edu.gt>
2020-02-22 20:40:11 -0600

    Inicializacion del proyecto
    Se inicio con la estructura basica del proyecto.

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const PORT = process.env.PORT || 3000;
+const db = require('./config/db');
+const app = express();
+
+
+app.use(express.json());
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+db.authenticate().then(() => console.log('Base de datos conectada...')).catch(err => console.log('Error' + err));
+
+// Routes
+//const auth = require('./routes/auth');
+
+// Mount routers
+//app.use('/api/v1/auth', auth);
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor corriendo en modo ${process.env.NODE_ENV} en el puerto ${process.env.PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.status(200);
+});
